0193036dac584d07fe355ef305c19bd90044b517
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-09-14 11:48:38 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,32 @@
 // add code snippets from README
+let state
+// let action = {
+//   type: "INCREASE"
+// }
+const countButton = document.getElementById("button")
+
+const reducer = (state={count: 0}, action) => {
+  console.log(state, action)
+  switch (action.type) {
+    case 'INCREASE':
+      return {count: state.count+1}
+    default:
+      return state
+  }
+}
+
+const dispatch = action => {
+  state = reducer(state, action)
+  // console.log('hi')
+  render()
+}
+
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
+ 
+dispatch({type: '@@INIT'})
+
+countButton.addEventListener('click', () => dispatch({type: 'INCREASE'}))
+// countButton.addEventListener('click', console.log('sup'))
