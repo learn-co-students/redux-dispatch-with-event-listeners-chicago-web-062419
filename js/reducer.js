ff7e7b90cdeb7cee64859f3f28664ba3117399b4
Erica Hawkins <ehawk901@gmail.com>
2019-09-18 16:38:50 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,22 @@
 // add code snippets from README
+
+ 
+function dispatch(action){
+    state = reducer(state, action)
+    render()
+}
+
+function render(){
+    let container = document.getElementById('container') 
+    container.textContent = state.count
+    
+} 
+  dispatch({type: '@@INIT'})
+
+let button = document.getElementById("button")
+
+button.addEventListener('click', function(){
+    dispatch({type: 'INCREASE_COUNT'})
+})
+
+
